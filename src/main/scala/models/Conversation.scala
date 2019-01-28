eb4137ced7cbf29c117e134f01b936a758196e07
anawatj <ajarusiripot@gmail.com>
2019-01-28 17:18:27 +0700

    conversation

--- src/main/scala/models/Conversation.scala
+++ src/main/scala/models/Conversation.scala
@@ -1,13 +1,23 @@
 package models
 
 trait ConversationType{
+  override def toString: String
 
 }
-object GroupConversationType extends ConversationType
-object UserConversationType extends ConversationType
+
+object GroupConversationType extends ConversationType {
+  override def toString: String = "Group"
+}
+
+object UserConversationType extends ConversationType{
+  override def toString: String = "User"
+}
 
 case class Conversation(id:String,conversation_name:String,conversation_type:ConversationType)
 
 case class ConversationUsers(conversation_id:String,user_id:String)
 
 case class ConversationMessage(conversation_id:String,message_id:String)
+
+
+
