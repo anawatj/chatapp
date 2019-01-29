ba58e8a158d4fa0a63a612d23506313afa76c327
anawatj <ajarusiripot@gmail.com>
2019-01-29 09:55:41 +0700

    conversation

--- src/main/scala/mappings/ConversationTable.scala
+++ src/main/scala/mappings/ConversationTable.scala
@@ -1,5 +1,23 @@
 package mappings
 
-trait ConversationTable {
+import databases.DatabaseComponent
+import models.{Conversation, ConversationType}
+import models.ConversationType._
+trait ConversationTable extends DatabaseComponent{
+
+  import profile.api._
+  implicit val conversationTypeMapper = MappedColumnType.base[ConversationType, String](
+    e => e.toString,
+    s => s
+  )
+
+  class ConversationTable(tag:Tag) extends Table[Conversation](tag,"conversations"){
+    def id=column[String]("id",O.PrimaryKey)
+    def conversation_name = column[String]("conversation_name")
+    def conversation_type = column[ConversationType]("conversation_type")
+
+    def * = (id,conversation_name,conversation_type)<>(Conversation.tupled,Conversation.unapply)
+
+  }
 
 }
--- src/main/scala/models/Conversation.scala
+++ src/main/scala/models/Conversation.scala
@@ -1,22 +1,21 @@
 package models
 
-trait ConversationType{
-  type  Result
-  def toRes:Result
+trait ConversationType {
+  def toString: String
 }
 
-object ConversationType{
+object ConversationType {
 
-    implicit def convertToConversation(str:String) = new ConversationType {
-      override type Result = String
-      override def toRes: String = str
-    }
+  implicit def convertToConversation(str: String) = new ConversationType {
+    override def toString: String = str
+  }
 }
-case class Conversation(id:String,conversation_name:String,conversation_type:ConversationType)
 
-case class ConversationUsers(conversation_id:String,user_id:String)
+case class Conversation(id: String, conversation_name: String, conversation_type: ConversationType)
 
-case class ConversationMessage(conversation_id:String,message_id:String)
+case class ConversationUsers(conversation_id: String, user_id: String)
+
+case class ConversationMessage(conversation_id: String, message_id: String)
 
 
 
