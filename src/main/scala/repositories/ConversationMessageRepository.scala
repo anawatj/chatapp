eb482c06b9c580a46b808c4e9dd3c37936241bfa
anawatj <ajarusiripot@gmail.com>
2019-01-30 11:04:58 +0700

    refactor repository

---
+++ src/main/scala/repositories/ConversationMessageRepository.scala
@@ -0,0 +1,42 @@
+package repositories
+
+import databases.MySqlComponent
+import mappings.{ConversationMessageTable, ConversationTable, MessageTable}
+import models.ConversationMessage
+
+import scala.concurrent._
+import ExecutionContext.Implicits.global
+import scala.concurrent.Future
+
+trait ConversationMessageRepository extends BaseRepository[ConversationMessage]{
+
+
+}
+
+class ConversationMessageRepositoryImpl extends ConversationMessageRepository with MySqlComponent with ConversationMessageTable with MessageTable with ConversationTable {
+
+  import profile.api._
+  override def add(data: ConversationMessage): Future[ConversationMessage] = {
+      db.run(ConversationMessages+=data) map {
+        _ => data
+      }
+  }
+
+  override def bulkAdd(list: List[ConversationMessage]): Future[List[ConversationMessage]] = {
+    db.run(ConversationMessages++=list) map {
+      _ => list
+    }
+  }
+
+  override def delete(id: String): Future[Unit] = {
+    null
+  }
+
+  override def find(id: String): Future[Option[ConversationMessage]] = {
+    null
+  }
+
+  override def update(data: ConversationMessage, id: String): Future[ConversationMessage] = {
+    null
+  }
+}
